1ee283c0087adecb5d303b6bc8a946ea747fe547
Ethan Anderson <ECA9001@gmail.com>
2026-02-16 18:41:33 -0700

    Removed readability due to popular demand

--- ProtfolioJam/Assets/Scripts/Fish.cs
+++ ProtfolioJam/Assets/Scripts/Fish.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     private Vector2 _moveDir;
-    [SerializeField] private float _swimSpeed, _maxSwimSpeed;
+    [SerializeField] private float swimSpeed, maxSwimSpeed;
 
     [SerializeField] private float waveHeight = 0.5f;
     [SerializeField] private float waveFrequency = 2f;
 
     Vector3 _spawnLocation;
 
-    public Collider2D _collider;
-    public Rigidbody2D _rb;
+    public Collider2D fishCollider;
+    public Rigidbody2D rb;
 
     
 
     private void Start()
     {
         _moveDir = this.transform.right;
-        _swimSpeed = 1;
+        swimSpeed = 1;
 
         _spawnLocation = transform.position;
 
@@ -26,18 +26,19 @@ public class Fish : MonoBehaviour
     private void Update()
     {
         Swim(transform.right);
+        Bob();
     }
 
     private void Swim(Vector2 dir_)
     {
-        _rb.AddForce(dir_);
+        rb.AddForce(dir_);
 
 
 
-        Vector2 tmp = _rb.linearVelocity;
+        Vector2 tmp = rb.linearVelocity;
 
-        tmp.x = Mathf.Clamp(tmp.x, -_maxSwimSpeed, _maxSwimSpeed);
-        _rb.linearVelocity = tmp;
+        tmp.x = Mathf.Clamp(tmp.x, -maxSwimSpeed, maxSwimSpeed);
+        rb.linearVelocity = tmp;
 
 
     }
@@ -45,6 +46,8 @@ public class Fish : MonoBehaviour
     {
         float newY = _spawnLocation.y + Mathf.Sin(Time.time * waveFrequency) * waveHeight;
         Vector3 pos = transform.position;
+        pos.y = newY;
+        transform.position = pos;
 
     }
 }
