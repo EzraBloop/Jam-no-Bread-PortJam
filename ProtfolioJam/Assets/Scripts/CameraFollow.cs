90175e01452ec0242c72d8aebb2004e7fe42e06d
Z-Moore548 <zmoore1@nait.ca>
2026-02-17 15:42:46 -0700

    cam

--- ProtfolioJam/Assets/Scripts/CameraFollow.cs
+++ ProtfolioJam/Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -7,6 +6,6 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        gameObject.transform.position = new Vector3(trackingTarget.transform.position.x, trackingTarget.transform.position.y, -10);
+        gameObject.transform.position = new Vector3(trackingTarget.transform.position.x, trackingTarget.transform.position.y - 2, -10);
     }
 }
