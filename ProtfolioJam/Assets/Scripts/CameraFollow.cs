e4729c1b78ac5da46019aa562de9dd3dbd1bbafc
EzraBloop <ezrajept@gmail.com>
2026-02-17 15:58:14 -0700

    updated camera follow

--- ProtfolioJam/Assets/Scripts/CameraFollow.cs
+++ ProtfolioJam/Assets/Scripts/CameraFollow.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject trackingTarget;
+    public float distanceFromObject;
 
     void Update()
     {
-        gameObject.transform.position = new Vector3(trackingTarget.transform.position.x, trackingTarget.transform.position.y - 2, -10);
+        gameObject.transform.position = new Vector3(trackingTarget.transform.position.x, trackingTarget.transform.position.y - 2, distanceFromObject);
     }
 }
